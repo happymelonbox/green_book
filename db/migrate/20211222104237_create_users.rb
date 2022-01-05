74ab9c1a669752e10f51aa61eb738e392432eb67
Justin Berger Howes <happymelonbox@gmail.com>
2022-01-05 21:49:32 +1000

    updated user info

--- db/migrate/20211222104237_create_users.rb
+++ db/migrate/20211222104237_create_users.rb
@@ -2,6 +2,15 @@ class CreateUsers < ActiveRecord::Migration[7.0]
   def change
     create_table :users do |t|
       t.string :username
+      t.string :first_name
+      t.string :last_name
+      t.string :contact_number
+      t.integer :address_unit_number
+      t.integer :address_street_number
+      t.string :address_street_name
+      t.string :address_suburb
+      t.string :address_city
+      t.string :address_country
       t.string :email
       t.string :password_digest
 
--- db/schema.rb
+++ db/schema.rb
@@ -110,6 +110,15 @@ ActiveRecord::Schema.define(version: 2022_01_05_112728) do
 
   create_table "users", force: :cascade do |t|
     t.string "username"
+    t.string "first_name"
+    t.string "last_name"
+    t.string "contact_number"
+    t.integer "address_unit_number"
+    t.integer "address_street_number"
+    t.string "address_street_name"
+    t.string "address_suburb"
+    t.string "address_city"
+    t.string "address_country"
     t.string "email"
     t.string "password_digest"
     t.datetime "created_at", precision: 6, null: false
