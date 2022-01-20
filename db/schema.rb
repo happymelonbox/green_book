359cdd5d3c225dfaab52a8b7a1b12e56bde4e2f1
Justin Berger Howes <happymelonbox@gmail.com>
2022-01-20 22:15:06 +1100

    added uid and provider to users

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_01_17_093003) do
+ActiveRecord::Schema.define(version: 2022_01_20_111431) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -148,6 +148,8 @@ ActiveRecord::Schema.define(version: 2022_01_17_093003) do
     t.string "password_digest"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "uid"
+    t.string "provider"
   end
 
   create_table "visits", force: :cascade do |t|
