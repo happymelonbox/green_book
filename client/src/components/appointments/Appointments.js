e80b93805e1a6b70b17a25010fb20c6d567374c1
Justin Berger Howes <happymelonbox@gmail.com>
2022-01-29 00:26:57 +1100

    appointmentsbychild edit complete

--- client/src/components/appointments/Appointments.js
+++ client/src/components/appointments/Appointments.js
@@ -64,9 +64,10 @@ class Appointments extends Component{
 
     handleAppointmentSubmit = (event) => {
         event.preventDefault()
+        
             let reason = event.target.appointment_reason.value ? event.target.appointment_reason.value : event.target.appointment_reason.defaultValue
             let other_reason = event.target.appointment_other_reason.value ? event.target.appointment_other_reason.value : null
-            let date_and_time = event.target.appointment_date_and_time.value ?  event.target.appointment_date_and_time.value :  event.target.appointment_date_and_time.defaultValue
+            let date_and_time = event.target.appointment_date_and_time.value !== "" ?  event.target.appointment_date_and_time.value :  event.target.appointment_date_and_time.defaultValue
             let location_name = event.target.appointment_location_name.value ?  event.target.appointment_location_name.value :  event.target.appointment_location_name.defaultValue
             let location_address_number = event.target.appointment_location_address_number.value ? event.target.appointment_location_address_number.value : event.target.appointment_location_address_number.defaultValue
             let location_street_name = event.target.appointment_location_street_name.value ? event.target.appointment_location_street_name.value : event.target.appointment_location_street_name.defaultValue
@@ -76,7 +77,13 @@ class Appointments extends Component{
             let location_state = event.target.appointment_location_state.value ? event.target.appointment_location_state.value : event.target.appointment_location_state.defaultValue
             let location_country = event.target.appointment_location_country.value ? event.target.appointment_location_country.value : event.target.appointment_location_country.defaultValue
             let location_contact_number = event.target.appointment_location_contact_number.value ? event.target.appointment_location_contact_number.value : event.target.appointment_location_contact_number.defaultValue
-            let visit_age = event.target.appointment_visit_age.value ? event.target.appointment_visit_age.value : event.target.appointment_visit_age.defaultValue
+            let visit_age =
+                reason === "MCHS Visit"
+                ?
+                event.target.appointment_visit_age.value
+                :
+                ""
+                console.log(visit_age)
             let child_id = event.target.appointment_child_id.value ? event.target.appointment_child_id.value : event.target.appointment_child_id.defaultValue
             let id = event.target.appointment_id.value ? event.target.appointment_id.value : event.target.appointment_id.defaultValue
 
@@ -94,7 +101,6 @@ class Appointments extends Component{
                 location_contact_number: location_contact_number,
                 visit_age: visit_age,
                 child_id: child_id,
-                id: id
             }
             axios.put(`http://localhost:3001/api/v1/appointments/${id}`, {appointment}, {withCredentials:true})
             .then(response=>{
@@ -128,9 +134,9 @@ class Appointments extends Component{
                 </select></label>
                 </form>
                 {this.state.sortBy === "Children" ?
-                    <AppointmentsByChild children={this.state.children} handleAppointmentSubmit={this.handleAppointmentSubmit} handleAppointmentChange={this.handleAppointmentChange} handleAppointmentEdit={this.handleAppointmentEdit}/>
+                    <AppointmentsByChild children={this.state.children} handleAppointmentSubmit={this.handleAppointmentSubmit} handleAppointmentEdit={this.handleAppointmentEdit}/>
                     :
-                    <AppointmentsByAppointment children={this.state.children} appointments={this.state.appointments} handleAppointmentSubmit={this.handleAppointmentSubmit} handleAppointmentChange={this.handleAppointmentChange} handleAppointmentEdit={this.handleAppointmentEdit}/>
+                    <AppointmentsByAppointment children={this.state.children} appointments={this.state.appointments} handleAppointmentSubmit={this.handleAppointmentSubmit} handleAppointmentEdit={this.handleAppointmentEdit}/>
                 }
             </div>
         )
--- client/src/components/appointments/AppointmentsByChild.js
+++ client/src/components/appointments/AppointmentsByChild.js
@@ -1,46 +1,82 @@
 import React from "react"
 
 
-const AppointmentsByChild = ({children}) => {
+const AppointmentsByChild = ({children, handleAppointmentSubmit, handleAppointmentEdit}) => {
     return(
         <div>
             {children.map(child => {
                 if(child.appointments.length > 0){
-                return(
-                    <div key={child.id}>
-                        <h5 className="child_names">{child.first_name} {child.last_name}</h5>
-                        <h5>Appointments:</h5>
-                        {child.appointments.map(appointment => {
-                            const date = appointment.date_and_time.split("T")[1].slice(0,5)
-                            const date_split = date.split("-")
-                            const day = date_split[2]
-                            const month = date_split[1]
-                            const year = date_split[0]
-                            const appointment_date = `${day}-${month}-${year}`
-                            return(
-                                <form key={appointment.id} className="appointment_details">
-                                    <label>Appointment Reason: {appointment.reason === "MCHS Visit"
-                                    ?
-                                        <label className="appointment_details_lines">{appointment.visit_age} {appointment.reason}<br/></label>
-                                    :
-                                        <label className="appointment_details_lines">{appointment.reason}<br/></label>
-                                    }</label>
-                                    <label className="appointment_details_lines">Date: {appointment.date_and_time.split("T")[0]}<br/></label>
-                                    <label className="appointment_details_lines">Time: {appointment_date}<br/></label><br/>
-                                    <label className="appointment_details_lines"><strong>Location:</strong><br/></label>
-                                    <label className="appointment_details_lines_location_details">
-                                        {appointment.location_name}<br/>
-                                        {appointment.location_address_number} {appointment.location_street_name}<br/>
-                                        {appointment.location_suburb}<br/>
-                                        {appointment.location_state} {appointment.location_postcode}
-                                    </label>
-                                    <input type="hidden" name="appointment_id"value={appointment.id}/>
-                                </form>
-                            )
-                        })}
-                    </div>
-                )}
+                    return(
+                        <div key={child.id}>
+                            <h5 className="child_names">{child.first_name} {child.last_name}</h5>
+                            <div><strong>Appointments:</strong><br/>
+                                {child.appointments.map(appointment => {
+                                    const date_and_time = appointment.date_and_time.split("T")
+                                    const time = date_and_time[1].slice(0,5)
+                                    const date_split = date_and_time[0].split("-")
+                                    const day = date_split[2]
+                                    const month = date_split[1]
+                                    const year = date_split[0]
+                                    const appointment_date = `${day}-${month}-${year}`
+                                    function handleReasonChange(event){
+                                        const value = event.target.value
+                                        const id = event.target.id.split("_")[0]
+                    
+                                        if(value === "Other"){
+                                            document.getElementById(`${id}other_reason_input`).classList.remove("hidden")
+                                            document.getElementById(`${id}visit_age_select`).classList.add("hidden")
+                                        } else if (value === "MCHS Visit"){
+                                            document.getElementById(`${id}visit_age_select`).classList.remove("hidden")
+                                            document.getElementById(`${id}other_reason_input`).classList.add("hidden")
+                                        }
+                                    }
+                                    return(
+                                        <div key={appointment.id}>
+                                            <form onSubmit={handleAppointmentSubmit}>
+                                                <label>Appointment Reason: {appointment.reason} {appointment.visit_age} <select defaultValue={appointment.reason} className={`${appointment.id}appointment_inputs hidden`} name="appointment_reason" id={`${appointment.id}_appointment_reason`} onChange={handleReasonChange}>
+                                                    <option value="MCHS Visit">MCHS Visit</option>
+                                                    <option value="GP">GP</option>
+                                                    <option value="Paediatrician">Paediatrician</option>
+                                                    <option value="Dentist">Dentist</option>
+                                                    <option value="Other">Other</option>
+                                                </select><br/></label>
+                                                <label id={`${appointment.id}visit_age_select`} className="hidden">Visit age: <select defaultValue={appointment.visit_age} name="appointment_visit_age" >
+                                                    <option value="First Home">First Home</option>
+                                                    <option value="Two Week">Two Week</option>
+                                                    <option value="Four Week">Four Week</option>
+                                                    <option value="Eight Week">Eight Week</option>
+                                                    <option value="Four Month">Four Month</option>
+                                                    <option value="Six Month">Six Month</option>
+                                                    <option value="Twelve Month">Twelve Month</option>
+                                                    <option value="Eighteen Month">Eighteen Month</option>
+                                                    <option value="Two Year">Two Year</option>
+                                                    <option value="Three and a Half Year">Three and a Half Year</option>
+                                                </select><br/></label>
+                                                <label className="hidden" id={`${appointment.id}other_reason_input`}>Input another reason: <input defaultValue={appointment.reason} name="appointment_other_reason" type="text"/><br/></label>
+                                                <label>Date: {appointment_date} <input className={`${appointment.id}appointment_inputs hidden`} defaultValue={appointment.date_and_time} type="datetime-local" name="appointment_date_and_time"/><br/></label>
+                                                <label>Time: {time}<br/></label><br/>
+                                                <label><strong>Location:</strong><br/></label>
+                                                    <label>{appointment.location_name}<input defaultValue={appointment.location_name} className={`${appointment.id}appointment_inputs hidden`} name="appointment_location_name" type="text" /><br/>
+                                                    {appointment.location_address_number} {appointment.location_street_name}<input defaultValue={appointment.location_address_number} className={`${appointment.id}appointment_inputs hidden`} type="text" name="appointment_location_address_number" /><input defaultValue={appointment.location_street_name} className={`${appointment.id}appointment_inputs hidden`} type="text" name="appointment_location_street_name" /><br/>
+                                                    {appointment.location_suburb}<input defaultValue={appointment.location_suburb} className={`${appointment.id}appointment_inputs hidden`} type="text" name="appointment_location_suburb" /><br/>
+                                                    {appointment.location_city} {appointment.location_state} {appointment.location_postcode}<input defaultValue={appointment.location_city} className={`${appointment.id}appointment_inputs hidden`} type="text" name="appointment_location_city" /><input defaultValue={appointment.location_state} className={`${appointment.id}appointment_inputs hidden`} name="appointment_location_state" type="text" /><input defaultValue={appointment.location_postcode} className={`${appointment.id}appointment_inputs hidden`} type="number" name="appointment_location_postcode" />
+                                                    </label><br/>
+                                                    <label>{appointment.location_country}<input defaultValue={appointment.location_country} className={`${appointment.id}appointment_inputs hidden`} name="appointment_location_country" type="text" /><br/></label>
+                                                    <label>{appointment.location_contact_number}<input defaultValue={appointment.location_contact_number} className={`${appointment.id}appointment_inputs hidden`} name="appointment_location_contact_number" type="text" /><br/></label>
+                                                    <input type="hidden" name="appointment_id" value={appointment.id}/>
+                                                    <input type="hidden" name="appointment_child_id" value={appointment.child_id}/>
+                                                    <button className={`${appointment.id}appointment_inputs hidden`} type="submit">Submit Changes</button>
+                                                </form>
+                                                <button id={`${appointment.id}appointment_inputs`} onClick={handleAppointmentEdit}>Edit appointment</button><br/>
+                                        </div>
+                                    )
+                                })}
+                            </div>
+                        </div>
+                    )
+                }
             })}
         </div>
-    )}
+    )
+}
 export default AppointmentsByChild
