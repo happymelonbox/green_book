ca3858cc9a624e347a2123f73c0bca29160a44d0
Justin Berger Howes <happymelonbox@gmail.com>
2022-02-13 22:50:30 +1100

    user validations updated and working with google Oauth

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,20 +7,27 @@ class User < ApplicationRecord
     has_many :births, through: :children
     has_many :fathers, through: :births
 
-    validates :email, presence: true
-    validates :email, uniqueness: true
+    validates :email, presence: true, on: :create
+    validates :email, uniqueness: true, on: :create
+    validates :username, presence: true, on: :create
+    validates :username, uniqueness: true, on: :create
+    validates :first_name, presence: true, on: :create
+    validates :last_name, presence: true, on: :create
+    validates :address_street_number, presence: true, on: :create
+    validates :address_street_name, presence: true, on: :create
+    validates :address_suburb, presence: true, on: :create
+    validates :address_city, presence: true, on: :create
+    validates :address_state, presence: true, on: :create
+    validates :address_country, presence: true, on: :create
 
-
-    validates :email, format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i }
+    validates :email, format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i, on: :create }
     validates :contact_number, format: { with: /\A(\+?\(61\)|\(\+?61\)|\+?61|\(0[1-9]\)|0[1-9])?( ?-?[0-9]){7,9}\z/, on: :create}
 
 
     def self.from_omniauth(response)
-        User.find_or_create_by!(uid: response[:uid], provider: response[:provider]) do |u|
+        User.find_or_create_by(uid: response[:uid], provider: response[:provider]) do |u|
             u.email = response[:info][:email]
             u.password = SecureRandom.hex(15)
-            u.first_name = response[:info][:first_name]
-            u.last_name = response[:info][:last_name]
         end
     end
 
